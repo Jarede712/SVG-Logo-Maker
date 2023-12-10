167ba1f59c1d9cb65e201445c5f6f411a7a334b0
Jarede712 <jaredeic@yahoo.com>
2023-12-09 22:46:02 -0800

    added comments to code and updated SVG logo

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// import inquirer, fs, and svg.js
 const inquirer = require('inquirer'); 
 const fs = require('fs');
 const { SVG, registerWindow } = require('@svgdotjs/svg.js');
 
+// Import svgdom to create SVG window
 let createSVGWindow;
 import('svgdom').then((svgdom) => {
     createSVGWindow = svgdom.createSVGWindow;
@@ -9,12 +11,14 @@ import('svgdom').then((svgdom) => {
     const document = window.document;
     registerWindow(window, document);
 
+    //  Validate color input
     function isValidColor(input) {
         const hexColorRegex = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i;
         const colorKeywords = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
         return hexColorRegex.test(input) || colorKeywords.includes(input.toLowerCase());
     }
 
+    // Questions to ask user
     const questions = [
         { type: 'input', name: 'text', message: 'Enter up to three characters:', validate: input => input.length <= 3 && input.length > 0 },
         { type: 'input', name: 'textColor', message: 'Enter text color (you can use keyword or hex code):', validate: isValidColor },
@@ -22,29 +26,31 @@ import('svgdom').then((svgdom) => {
         { type: 'input', name: 'shapeColor', message: 'Enter a shape color (you can use keyword or hex code):', validate: isValidColor },
     ];
 
+    // Generate SVG based on user input
     function generateSVG({ text, textColor, shape, shapeColor }) {
         const canvas = SVG().size(300, 200);
         let shapeElement;
-        let shapeSize = 100; // Default shape size
-        let textSize = shapeSize / 3; // Calculate text size as a fraction of shape size
+        let shapeSize = 100; 
+        let textSize = shapeSize / 3; 
 
+        // Adjust shape size and text size based on shape type
         switch (shape) {
             case 'circle':
-                shapeElement = canvas.circle(shapeSize).fill(shapeColor).cx(150).cy(100);
+                shapeElement = canvas.circle(shapeSize).fill(shapeColor).cx(150).cy(100); // Create circle
                 break;
             case 'triangle':
-                const triangleHeight = Math.sqrt(3) / 2 * shapeSize;
-                shapeElement = canvas.polygon(`0,${triangleHeight} ${shapeSize / 2},0 ${shapeSize},${triangleHeight}`).fill(shapeColor);
+                const triangleHeight = Math.sqrt(3) / 2 * shapeSize; // Calculate height of triangle
+                shapeElement = canvas.polygon(`0,${triangleHeight} ${shapeSize / 2},0 ${shapeSize},${triangleHeight}`).fill(shapeColor); // Create triangle
                 shapeElement.cx(150).cy(100);
                 break;
             case 'square':
-                shapeElement = canvas.rect(shapeSize, shapeSize).fill(shapeColor).cx(150).cy(100);
+                shapeElement = canvas.rect(shapeSize, shapeSize).fill(shapeColor).cx(150).cy(100); // Create square
                 break;
         }
 
-        // Get the bounding box of the shape for positioning the text
         const bbox = shapeElement.bbox();
 
+        // Create text element and center it in the shape
         const textElement = canvas.text(text)
             .fill(textColor)
             .font({ size: textSize })
@@ -54,16 +60,17 @@ import('svgdom').then((svgdom) => {
             });
 
         if (shape === 'triangle') {
-            // For a triangle, adjust the text position manually a bit to cater for visual centering
-            textElement.center(bbox.cx, bbox.cy + textSize / 3);
+
+            textElement.center(bbox.cx, bbox.cy + textSize / 3); // Adjust text position for triangle
         } else {
-            // For circle and square, we can center the text directly
-            textElement.center(bbox.cx, bbox.cy);
+
+            textElement.center(bbox.cx, bbox.cy); // Center text in shape
         }
 
-        fs.writeFileSync('logo.svg', canvas.svg());
+        fs.writeFileSync('logo.svg', canvas.svg()); // Write SVG to file
     }
 
+    // Ask user questions and generate SVG
     inquirer.prompt(questions).then(answers => {
         generateSVG(answers);
         console.log('Generated logo.svg successfully!')
