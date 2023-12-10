82beaa693aadd05c796c9f5e78a6da8f72eacaa5
Jarede712 <jaredeic@yahoo.com>
2023-12-09 16:00:00 -0800

    fixed issues with triangle logo not generating correctly

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// install required packages: npm install inquirer svg.js svgdom
 const inquirer = require('inquirer'); 
 const fs = require('fs');
 const { SVG, registerWindow } = require('@svgdotjs/svg.js');
@@ -6,20 +5,15 @@ const { SVG, registerWindow } = require('@svgdotjs/svg.js');
 let createSVGWindow;
 import('svgdom').then((svgdom) => {
     createSVGWindow = svgdom.createSVGWindow;
-    const window = createSVGWindow(); // create a new window instance
-    const document = window.document; // get the document object from window
-    registerWindow(window, document); // register window and document
+    const window = createSVGWindow();
+    const document = window.document;
+    registerWindow(window, document);
 
     function isValidColor(input) {
-    // Regular expression to check if it's a valid hex color code
-    const hexColorRegex = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i;
-
-    // List of common color keywords
-    const colorKeywords = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
-
-    // Check if the input is a valid hex code or a color keyword
-    return hexColorRegex.test(input) || colorKeywords.includes(input.toLowerCase());
-}
+        const hexColorRegex = /^#?([a-f0-9]{6}|[a-f0-9]{3})$/i;
+        const colorKeywords = ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 'silver', 'teal', 'white', 'yellow'];
+        return hexColorRegex.test(input) || colorKeywords.includes(input.toLowerCase());
+    }
 
     const questions = [
         { type: 'input', name: 'text', message: 'Enter up to three characters:', validate: input => input.length <= 3 && input.length > 0 },
@@ -30,64 +24,45 @@ import('svgdom').then((svgdom) => {
 
     function generateSVG({ text, textColor, shape, shapeColor }) {
         const canvas = SVG().size(300, 200);
-    
         let shapeElement;
+        let shapeSize = 100; // Default shape size
+        let textSize = shapeSize / 3; // Calculate text size as a fraction of shape size
+
         switch (shape) {
             case 'circle':
-                shapeElement = canvas.circle(100).fill(shapeColor);
+                shapeElement = canvas.circle(shapeSize).fill(shapeColor).cx(150).cy(100);
                 break;
             case 'triangle':
-                shapeElement = canvas.polygon('0,100 50,0 100,100').fill(shapeColor);
+                const triangleHeight = Math.sqrt(3) / 2 * shapeSize;
+                shapeElement = canvas.polygon(`0,${triangleHeight} ${shapeSize / 2},0 ${shapeSize},${triangleHeight}`).fill(shapeColor);
+                shapeElement.cx(150).cy(100);
                 break;
             case 'square':
-                shapeElement = canvas.rect(100, 100).fill(shapeColor);
+                shapeElement = canvas.rect(shapeSize, shapeSize).fill(shapeColor).cx(150).cy(100);
                 break;
         }
-        shapeElement.cx(150).cy(100);
-    
+
+        // Get the bounding box of the shape for positioning the text
+        const bbox = shapeElement.bbox();
+
         const textElement = canvas.text(text)
             .fill(textColor)
-            .font({ size: 60 })
-            .attr({ 'text-anchor': 'middle' });
-        
-        // Center the text based on the center of the canvas, not the bounding box of the text
-        const textWidth = textElement.length();
-        const textHeight = textElement.font('size');
-    
-        // Center the text by setting the x and y position
-        textElement.cx(150).cy(100 + textHeight / 2);
-    
-        fs.writeFileSync('logo.svg', canvas.svg());
-    }function generateSVG({ text, textColor, shape, shapeColor }) {
-        const canvas = SVG().size(300, 200);
-    
-        let shapeElement;
-        switch (shape) {
-            case 'circle':
-                shapeElement = canvas.circle(100).fill(shapeColor);
-                break;
-            case 'triangle':
-                shapeElement = canvas.polygon('0,100 50,0 100,100').fill(shapeColor);
-                break;
-            case 'square':
-                shapeElement = canvas.rect(100, 100).fill(shapeColor);
-                break;
+            .font({ size: textSize })
+            .attr({
+                'text-anchor': 'middle',
+                'dominant-baseline': 'middle'
+            });
+
+        if (shape === 'triangle') {
+            // For a triangle, adjust the text position manually a bit to cater for visual centering
+            textElement.center(bbox.cx, bbox.cy + textSize / 3);
+        } else {
+            // For circle and square, we can center the text directly
+            textElement.center(bbox.cx, bbox.cy);
         }
-        shapeElement.cx(150).cy(100);
-    
-        const textElement = canvas.text(text)
-            .fill(textColor)
-            .font({ size: 60, anchor: 'middle' })
-            .move(150, 100);
-    
-        // Get the bounding box of the text element
-        const bbox = textElement.bbox();
-        // Adjust the position of the text element based on its bounding box
-        textElement.move(150 - bbox.width / 2, 100 - bbox.height / 2);
-    
+
         fs.writeFileSync('logo.svg', canvas.svg());
     }
-    
 
     inquirer.prompt(questions).then(answers => {
         generateSVG(answers);
