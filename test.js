6c78cbc870a26d12eddc589e43102f665ab26b47
Jarede712 <jaredeic@yahoo.com>
2023-12-12 17:27:53 -0800

    removed packagelock, added jest, tried running tests but the tests failed

---
+++ test.js
@@ -0,0 +1,7 @@
+const triangle = require('./index.js');
+
+
+  const shape = new triangle();
+  shape.setColor('blue');
+  expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+
